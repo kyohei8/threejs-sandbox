2daacf09cc472bff440fca3f96d294bb9fc8121c
kyohei <tsukuda.kyouhei@gmail.com>
2015-03-07 16:19:05 +0900

    update template generator

--- template-generator.js
+++ template-generator.js
@@ -8,15 +8,68 @@ if(!dir){
 }
 let dirPath = `${BASE_DIR}/${dir}`;
 let jsTemplate =
-  `require('../app.styl');
+  `'use strict';
+require('../app.styl');
 let THREE = require('three');
+let OrbitControls = require('three-orbit-controls')(THREE);
 let Stats = require('stats-js');
 
-let stats = new Stats();
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.top = '0px';
-document.body.appendChild( stats.domElement );
-//stats.update();`;
+let container,stats;
+let camera, controls, scene, renderer;
+
+let init = function(){
+  container = document.getElementById('container');
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 4000 );
+  camera.position.z = 1750;
+  controls = new OrbitControls( camera, container );
+
+  // insert your creativity :D
+
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  container.appendChild( renderer.domElement );
+
+  // set stats
+  stats = new Stats();
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild( stats.domElement );
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
+};
+
+let onWindowResize = function(){
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+};
+
+let animate = function(){
+
+  // insert your creativity :D
+
+  requestAnimationFrame(animate);
+  stats.update();
+  render();
+
+};
+
+let render = function(){
+
+  renderer.render( scene, camera );
+
+};
+
+init();
+animate();`;
 
 let htmlTemplate =
   `<!DOCTYPE html>
@@ -25,6 +78,7 @@ let htmlTemplate =
   <title>${dir}</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+  <script src="/node_modules/three/three.min.js"></script>
 </head>
 <body>
 <div id="container"></div>
