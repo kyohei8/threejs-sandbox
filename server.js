6e0d95056540b5994222c96dcdd04e287db09658
kyohei <tsukuda.kyouhei@gmail.com>
2015-03-15 16:04:48 +0900

    update server config

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var serveIndex = require('serve-index');
 var auth = require('basic-auth');
 
 var ba = {
-  name:'test',
-  pass:'test'
+  name: 'test',
+  pass: 'test'
 };
 
 var app = connect();
+
+app.use(serveStatic(__dirname));
+app.use(serveIndex(__dirname, {
+  filter: function(filename, index, files, dir){
+    // exなんちゃらで始まるディレクトリのみ表示する
+    return filename.indexOf('ex') > -1;
+  },
+  icons: true,
+  hidden: true,
+  view: 'details'
+}));
+
 app.use(function(req, res, next){
   var credentials = auth(req);
   if(!credentials || credentials.name !== ba.name || credentials.pass !== ba.pass){
@@ -19,5 +32,5 @@ app.use(function(req, res, next){
     next();
   }
 });
-app.use(serveStatic(__dirname));
 app.listen(process.env.PORT || 3000);
+console.log("Server Started");
